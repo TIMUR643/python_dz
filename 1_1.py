3edd8e75f874b490e36f64baddecb7a26c3205c8
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-04-23 10:02:07 +0300

    Create 1_1.py

---
+++ 1_1.py
@@ -0,0 +1,8 @@
+a = 10
+b = 15
+print(a, b)
+string1 = input("введите первую строку ")
+number1 = int(input("введите первое число "))
+string2 = input("введите вторую строку ")
+number2 = int(input("введите второе число "))
+print("введенные данные "+string1,  string2,number1,  number2)
