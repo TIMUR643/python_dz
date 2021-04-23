ba9537505d061bf8abf9d0a6d69c79b012628c75
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-04-23 17:47:08 +0300

    Update 1_5.py

--- 1_5.py
+++ 1_5.py
@@ -1,12 +1,12 @@
 profit = float(input("Введите выручку фирмы "))
 costs = float(input("Введите издержки фирмы "))
-profitability = profit / costs
+profitability = profit/(profit - costs)
 if profit > costs:
     print("Фирма работает с прибылью. Рентабельность выручки составила" )
     print(profitability)
     workers = int(input("Введите количество сотрудников фирмы "))
     profit1 = profit / workers
-    print("прибыль в расчете на одного сторудника сотавила")
+    print("прибыль в расчете на одного сотрудника сотавила")
     print(profit1)
 elif profit == costs:
     print("Фирма работает в ноль")
