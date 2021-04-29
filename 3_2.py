054fae0cecccb8e47212b71dedeaecd101f83dc4
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-04-29 09:57:48 +0300

    Create 3_2.py

---
+++ 3_2.py
@@ -0,0 +1,5 @@
+
+def div(name, surname, ear, city, email, phone):
+    print(name, surname, ear, city, email, phone)
+
+div(name= 'Timur', surname='Abdikarimov', ear=2005, city='Saint-Petersburg', email='timtim0975@gmail.com', phone='0232')
