e31cdbdbfba808b3b1a1cf561592fc17d8e14a18
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-05-01 10:12:32 +0300

    Update 3_2.py

--- 3_2.py
+++ 3_2.py
@@ -1,5 +1,5 @@
 
-def div(name, surname, ear, city, email, phone):
-    print(name, surname, ear, city, email, phone)
+def div(name, surname, year, city, email, phone):
+    print(name, surname, year, city, email, phone)
 
-div(name= 'Timur', surname='Abdikarimov', ear=2005, city='Saint-Petersburg', email='timtim0975@gmail.com', phone='0232')
+div(name= 'Timur', surname='Abdikarimov', year=2005, city='Saint-Petersburg', email='timtim0975@gmail.com', phone='0232')
