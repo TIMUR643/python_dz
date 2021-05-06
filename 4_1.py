f0442bbeefbf87147d0e37c01ea8b14391d5013e
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-05-06 18:03:51 +0300

    Delete 4_1.py

--- 4_1.py
+++
@@ -1,7 +0,0 @@
-def script():
-    time=float(input('Введите время работы в часах : '))
-    salary=float(input('Введите зарплату в час : '))
-    bonus=float(input('Введите премию - '))
-    pay=time*salary+bonus
-    return pay
-print(f'Размер зарплаты составил: {script()}')
