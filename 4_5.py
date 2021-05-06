3e38d779d5bdd4b68c00c04a1eef6f3058ea6039
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-05-06 18:02:02 +0300

    Delete 4_5.py

--- 4_5.py
+++
@@ -1,4 +0,0 @@
-from functools import reduce
-def my_func(el_p, el):
-    return el_p * el
-print(f'Результат перемножения всех элементов списка {reduce(my_func, [el for el in range(99, 1001) if el % 2 == 0])}')
