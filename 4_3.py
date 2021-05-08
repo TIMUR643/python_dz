6399ed3506f5865ec0c4dcb065ffcbd46c477630
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-05-08 17:23:29 +0300

    Update 4_3.py

--- 4_3.py
+++ 4_3.py
@@ -1,4 +1,1 @@
-for i in range(20, 240,20):
-    print(i)
-for a in range(20,240,21):
-    print(a)
+print(f'Числа от 20 до 240 кратные 20 или 21 -{ [el for el in range(20, 241) if el%20==0 or el%21==0]}')
