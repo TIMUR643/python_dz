cb73c17145fbd5070b748b2494b5361c8c1040bd
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-05-14 20:40:35 +0300

    Create 6_3.py

---
+++ 6_3.py
@@ -0,0 +1,25 @@
+
+class Worker:
+    name = None
+    surname = None
+    position = None
+    _income = {'wage': 'wage',
+               'bonus': 'bonus'
+               }
+
+class Position(Worker):
+    def __init__(self, name:str, surname:str, wage:int, bonus:int):
+        self.name = name
+        self.surname = surname
+        self._income['wage'] = wage
+        self._income['bonus'] = bonus
+
+    def get_full_name(self):
+        return self.name + self.surname
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+position = Position(name="Timur", surname="Abdikarimov", wage=100000, bonus=40000)
+
+print(f'{position.get_full_name(),position.get_total_income()}')
