705bac09b9f154bfc6e5e5cb1b03507884758e4f
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-05-17 11:28:17 +0300

    Update 6_3.py

--- 6_3.py
+++ 6_3.py
@@ -15,7 +15,7 @@ class Position(Worker):
         self._income['bonus'] = bonus
 
     def get_full_name(self):
-        return self.name + self.surname
+        return self.name  + " " + self.surname
 
     def get_total_income(self):
         return self._income['wage'] + self._income['bonus']
