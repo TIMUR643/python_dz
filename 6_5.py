d19bbc191a9fddd8232ba1a5e87ab52df501160b
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-05-17 11:24:10 +0300

    Update 6_5.py

--- 6_5.py
+++ 6_5.py
@@ -1,28 +1,25 @@
 class Stationery:
-    atr_title = 'Title'
+    title = 'Title'
     def draw(self):
         print('Запуск отрисовки.')
 class Pen(Stationery):
     title = 'Pen'
     def draw(self):
-        print('Запуск отрисовки')
-        return self.title
+        print('Запуск отрисовки ' + self.title)
 
 class Pencil(Stationery):
     title = 'Pencil'
     def draw(self):
-        print('Запуск отрисовки')
-        return self.title
+        print('Запуск отрисовки ' + self.title)
 
 class Handle(Stationery):
     title = 'Handle'
     def draw(self):
-        print('Запуск отрисовки')
-        return self.title
+        print('Запуск отрисовки ' + self.title)
 
 my_pen = Pen()
 my_pencil = Pencil()
 my_handle = Handle()
-print(my_pen.draw())
-print(my_pencil.draw())
-print(my_handle.draw())
+my_pen.draw()
+my_pencil.draw()
+my_handle.draw()
